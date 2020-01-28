7a03db48269aacc8533b859ef8cdaca4d2927120
AkhilMittal1 <55084935+AkhilMittal1@users.noreply.github.com>
2020-01-28 14:55:26 +0530

    1st program added

---
+++ akhil.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+
+int a,b;
+
+printf("enter the first number");
+scanf("%d",&a);
+
+printf("enter the second number ");
+scanf("%d",&b);
+
+a>b?printf("the greatest number is %d",a): printf("the greatest number is %d",b);
+
+}
+
